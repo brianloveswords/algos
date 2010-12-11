23f6eba8ef5475fb239738d8f50288879e7e0d4b
Brian Brennan <brianloveswords@gmail.com>
2010-12-11 15:49:23 -0500

    Docco compatible comments.

--- distance.js
+++ distance.js
@@ -1,3 +1,13 @@
+// String Distance
+//
+// Developed by Brian J. Brennan 
+// Copyright (c) 2010
+// MIT license
+
+// Compute a modified Hamming distance. The standard Hamming distance requires
+// the strings to be the same length. This version can take strings of any
+// size and applies the difference in length to the distance, so `ham` and
+// `hamsled` will have a distance of 4.
 var hamming = function(str1, str2){
   var dist = 0,
       big = str1,
